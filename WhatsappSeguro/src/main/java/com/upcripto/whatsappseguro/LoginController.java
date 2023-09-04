f4b0e1ea1f7987fe0923b54d9a206055f56a6f89
Sebastian Fernandez <seb.flipflip@gmail.com>
2023-09-04 15:31:10 -0600

    works fine commit

--- WhatsappSeguro/src/main/java/com/upcripto/whatsappseguro/LoginController.java
+++ WhatsappSeguro/src/main/java/com/upcripto/whatsappseguro/LoginController.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Cursor;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class LoginController {
@@ -15,6 +18,25 @@ public class LoginController {
     TextField numInput;
 
     @FXML
+    private Button LoginBtn;
+
+    public void initialize() {
+        // Initialize your button and set event listeners in the initialize method
+        LoginBtn.setCursor(Cursor.DEFAULT);
+
+        LoginBtn.setOnMouseExited(event -> {
+            LoginBtn.setCursor(Cursor.DEFAULT);
+        });
+
+        LoginBtn.setOnMouseEntered(event -> {
+            LoginBtn.setCursor(Cursor.HAND);
+        });
+    }
+
+    @FXML
+    Label loginError;
+
+    @FXML
     private void LoginRegister(ActionEvent event) throws IOException{
         ConnectionsController.ValidateLogin(nameInput.getText(), numInput.getText());
     }
@@ -23,11 +45,17 @@ public class LoginController {
         int user_id = response;
         if(user_id==-1){
             System.out.println("Número ya esta registrado y nombre no coincide!");
+            //LoginError();
             return;
         }
         ContactosController.setUserId(user_id);
         System.out.println("Sesion iniciada para user id "+user_id);
         App.setRoot("contacts");
 
+    }
+    
+    @FXML
+    private void LoginError(){
+        loginError.setVisible(true);
     }
 }
